bac788ca564a2e006c8d29dc0e475e3b62f5c54e
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-01-11 06:32:12 -1100

    React-App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import "./styles.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Bello", id: "1", time: "3: 30 PM" },
+      { name: "Max", id: "2", time: "4: 30 PM" },
+      { name: "Manu", id: "3", time: "5: 20 PM" }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        <Person name="Bello" id="1" time="3: 30 PM" />
-        <Person name="Max" id="2" time="4: 30 PM" />
-        <Person name="Manu" id="3" time="5: 20 PM" />
+        <Person
+          name={this.state.persons[0].name}
+          id={this.state.persons[0].id}
+          time={this.state.persons[0].time}
+        />
+        <Person
+          name={this.state.person[1].name}
+          id={this.state.persons[1].id}
+          time={this.state.persons[1].time}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          id={this.state.persons[2].id}
+          time={this.state.persons[2].time}
+        />
       </div>
     );
   }
